79e894f31a403daf6657270d5b838cedaf878c8c
maigiordano28 <309728@fi365.ort.edu.uy>
2024-10-31 20:26:12 -0300

    aa
    a

---
+++ ObligatorioDDA/src/Observador/observable.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Observador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author HOLA
+ */
+public abstract class observable {
+    private final List<observador> observadores = new ArrayList<>();
+    
+    
+}
---
+++ ObligatorioDDA/src/Observador/observador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Observador;
+
+/**
+ *
+ * @author HOLA
+ */
+public interface observador {
+    public void actualizar(observable o,Object evento);
+}
