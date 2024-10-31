4f69a70a935be129d3f8f8db435129d003a38106
maigiordano28 <309728@fi365.ort.edu.uy>
2024-10-31 20:30:27 -0300

    rr
    rr

--- ObligatorioDDA/src/Observador/observable.java
+++ ObligatorioDDA/src/Observador/observable.java
@@ -15,4 +15,24 @@ public abstract class observable {
     private final List<observador> observadores = new ArrayList<>();
     
     
+    
+    
+      public void agregar(observador observador) {
+        if (!observadores.contains(observador)) {
+            observadores.add(observador);
+        }
+    }
+    
+    public void quitar(observador observador) {
+        if (observadores.contains(observador)) {
+            observadores.remove(observador);
+        }
+    }
+    
+    public void avisar(Object evento) {
+        List<observador> observadoresTemporal = new ArrayList<>(observadores);
+        for (observador observador : observadoresTemporal) {
+            observador.actualizar(this, evento);
+        }
+    }
 }
